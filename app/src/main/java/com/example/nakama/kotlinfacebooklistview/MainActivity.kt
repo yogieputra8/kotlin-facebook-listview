a063aab142c4ac162bdd0aea1434515fce014d9b
yogieputra8 <yogie.putra8@gmail.com>
2017-09-03 16:09:38 +0700

    Custom Listview

--- app/src/main/java/com/example/nakama/kotlinfacebooklistview/MainActivity.kt
+++ app/src/main/java/com/example/nakama/kotlinfacebooklistview/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.nakama.kotlinfacebooklistview
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
@@ -22,15 +23,28 @@ class MainActivity : AppCompatActivity() {
 
     private class MyCustomAdapter(context: Context): BaseAdapter(){
         private val mContext: Context
+        private val names = arrayListOf<String>(
+                "Donald Trump", "Steve Jobs", "Tim Cook", "Mark Zuckerberg", "Joko Widodo"
+        )
 
         init {
             mContext = context
         }
 
         override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
-            val textView = TextView(mContext)
-            textView.text = "Here is my row for Listview"
-            return textView
+            val layoutInflater = LayoutInflater.from(mContext)
+            val rowMain = layoutInflater.inflate(R.layout.row_main, viewGroup, false)
+
+            val nameTextView = rowMain.findViewById<TextView>(R.id.name_textView)
+            nameTextView.text = names.get(position)
+
+            val positionTextView = rowMain.findViewById<TextView>(R.id.position_textview)
+            positionTextView.text = "Row number: $position"
+
+            return rowMain
+            // val textView = TextView(mContext)
+            // textView.text = "Here is my row for Listview"
+            // return textView
         }
 
         override fun getItem(position: Int): Any {
@@ -42,7 +56,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun getCount(): Int {
-            return 5
+            return names.size
         }
 
     }
